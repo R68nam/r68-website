82d5b52364d9f22de019392bb343ae9af43caa45
NamNguyen52 <nam.nguyen52@gmail.com>
2016-10-14 18:22:59 -0700

    minor changes to placeholder contact page

---
+++ src/components/contact/contact.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import TweenMax from 'gsap';
+
+class Contact extends Component {
+  render() {
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col-sm-12">
+            <h1>Contact Us With Your Billion Dollar Idea</h1>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export deault Contact;
